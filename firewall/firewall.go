6105d6c7967da496853299cad948821069bf39eb
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-03-02 14:45:39 +0100

    Fix: use br name everywhere

--- firewall/firewall.go
+++ firewall/firewall.go
@@ -107,8 +107,8 @@ func (f *Firewall) Setup() (err error) {
 	defaults := []Rule{
 		{raw: "-m conntrack --ctstate ESTABLISHED -j ACCEPT", append: true},
 		{raw: fmt.Sprintf("-i %s -j DROP", f.outInterface), append: true},
-		{raw: fmt.Sprintf("-o %s -p tcp --dport 53 -j ACCEPT", f.bridge), append: false},
-		{raw: fmt.Sprintf("-o %s -p udp --dport 53 -j ACCEPT", f.bridge), append: false},
+		{raw: fmt.Sprintf("-o %s -p tcp --dport 53 -j ACCEPT", f.outInterface), append: false},
+		{raw: fmt.Sprintf("-o %s -p udp --dport 53 -j ACCEPT", f.outInterface), append: false},
 	}
 
 	for _, rule := range defaults {
